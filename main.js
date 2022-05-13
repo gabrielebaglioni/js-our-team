0d1dca4866b4c6e0a5f0eeba6ea5be0f23432b01
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-13 15:13:00 +0200

    logica in fase di debug

---
+++ main.js
@@ -0,0 +1,24 @@
+//creo una lista di ogetti "card"
+//cre gli ogetti 
+//img
+//name
+//ruolo
+const card = [
+   {
+      name: "wayne Barret",
+      ruolo: "Founder / CEO",
+      img : "img/wayne-barnett-founder-ceo.jpg"
+   },
+   {
+      name: "wayne Barret",
+      ruolo: "Founder / CEO",
+      img : "img/wayne-barnett-founder-ceo.jpg"
+   }
+];
+// prendo il container html dove devo stampare i miei ogetti
+const cardContainer = document.querySelector(".team-card")
+//ciclo la mia lista di ogetti 
+for(let i = 0; i < card.length; i++){
+   const card =`
+   `;
+}
